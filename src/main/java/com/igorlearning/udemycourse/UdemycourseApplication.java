77a1e80120ec5d1a8201c8ed9ab888c6688cf1a3
Igor <igorferreiradaniel99@hotmail.com>
2022-02-21 22:42:23 -0300

    Project created

---
+++ src/main/java/com/igorlearning/udemycourse/UdemycourseApplication.java
@@ -0,0 +1,13 @@
+package com.igorlearning.udemycourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemycourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemycourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igorlearning/udemycourse/UdemycourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igorlearning.udemycourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemycourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
