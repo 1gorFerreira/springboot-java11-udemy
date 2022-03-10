25d10c7a315b5bef4827ee47483758326767088a
Igor Ferreira <igorferreiradaniel99@hotmail.com>
2022-03-09 23:51:21 -0300

    Revert "OrderStatus enum"
    This reverts commit 874cb8bebe4c1c88be0e31e551f5136561a2c07b.

--- src/main/java/com/igorlearning/udemycourse/entities/Order.java
+++ src/main/java/com/igorlearning/udemycourse/entities/Order.java
@@ -13,7 +13,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.igorlearning.udemycourse.entities.enums.OrderStatus;
 
 @Entity
 @Table(name = "tb_order")
@@ -27,19 +26,16 @@ public class Order implements Serializable{
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
-	private Integer orderStatus;
-	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
 	
 	public Order() {}
 
-	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+	public Order(Long id, Instant moment, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
-		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -57,16 +53,6 @@ public class Order implements Serializable{
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
-	}
-
-	public OrderStatus getOrderStatus() {
-		return OrderStatus.valueOf(orderStatus) ;
-	}
-
-	public void setOrderStatus(OrderStatus orderStatus) {
-		if (orderStatus != null) {
-			this.orderStatus = orderStatus.getCode();	
-		}
 	}
 
 	public User getClient() {
