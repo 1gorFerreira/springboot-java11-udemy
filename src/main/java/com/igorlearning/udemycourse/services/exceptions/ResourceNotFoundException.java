81573fac9cb277ea6bb7ed6750ffc1cb07477a7e
Igor <igorferreiradaniel99@hotmail.com>
2022-04-04 12:10:18 -0300

    Exception handling - findById

---
+++ src/main/java/com/igorlearning/udemycourse/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.igorlearning.udemycourse.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(Object id) {
+		super("Resource Not Found. Id " + id);
+	}
+}
